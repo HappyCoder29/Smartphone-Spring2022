7335eec70da128ac3c344ff189be57f338c91a36
Ashish <ashish@drillmaps.com>
2022-01-28 18:14:29 -0800

    Checking in Project for Hello world

---
+++ HelloWorld/HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Ashish Ashish on 1/28/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
